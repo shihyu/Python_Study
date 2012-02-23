972a3d3150f2ac474e9a7a54a26c30ed262f7dbf
sisimi.pchome <sisimi.pchome@gmail.com>
2012-02-23 14:39:10 +0000

    更新下載路徑

--- example/ChromiumDownload/ChromiumDownload.py
+++ example/ChromiumDownload/ChromiumDownload.py
@@ -1,4 +1,7 @@
 import urllib
-url = "http://build.chromium.org/f/chromium/snapshots/Win_Webkit_Latest/"
-build_id = urllib.urlopen(url + "LATEST").read()
+#url = "http://build.chromium.org/f/chromium/snapshots/Win_Webkit_Latest/"
+url = 'http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/'
+build_id = urllib.urlopen(url + "LAST_CHANGE").read()
 urllib.urlretrieve (url +  build_id + '/chrome-win32.zip', "chrome-win32("+build_id+").zip")
+#urllib.urlretrieve ('http://download-chromium.appspot.com/dl/Win', "chrome-win32.zip")
+
